356384f7cded94fdbca51724d9ceefb87884df73
VVRazum <ky3@list.ru>
2024-05-30 22:56:33 +0300

    KS-3-1 Welcome Pedro

---
+++ src/main/kotlin/Lesson_3/Lesson3_Task1.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val userName = "Педро"
+    val dayWelcome = "Добрый день, уважаемый"
+    val eveWelcome = "Добрый вечер, уважаемый"
+
+    println("$dayWelcome $userName!")
+    println("$eveWelcome $userName!")
+
+
+}
+
