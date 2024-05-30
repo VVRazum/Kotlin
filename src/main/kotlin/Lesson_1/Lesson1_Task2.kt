8e7a6f436941f2e3fd339211fa530693ae36eaec
VVRazum <ky3@list.ru>
2024-05-30 10:39:03 +0300

    KS-1-2

--- src/main/kotlin/Lesson_1/Lesson1_Task2.kt
+++ src/main/kotlin/Lesson_1/Lesson1_Task2.kt
@@ -1,13 +1,13 @@
 fun main() {
-    var totalOrders: Int = 75
+    val totalOrders: Int = 75
     val thanksPhrase: String = "Спасибо за заказ в нашем магазине!"
 
     println(totalOrders)
     println(thanksPhrase)
 
-    //var totalWorkers: Int = 2000
+    var totalWorkers: Int = 2000
     //println(totalWorkers)
-    var totalWorkers: Int = 1999
+    totalWorkers = 1999
     println(totalWorkers)
 }
 
